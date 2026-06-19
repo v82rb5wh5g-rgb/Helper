233d8c20bc524adaf45058b0bd39ef1f180fcc20
v82rb5wh5g-rgb <v82rb5wh5g@privaterelay.appleid.com>
2026-06-19 06:34:14 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { token } = require("./config");
+const redis = require("./redis");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+  partials: [
+    Partials.Message,
+    Partials.Channel,
+    Partials.Reaction,
+  ]
+});
+
+// Load events
+const fs = require("fs");
+const path = require("path");
+const eventsPath = path.join(__dirname, "events");
+if (fs.existsSync(eventsPath)) {
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+  for (const file of eventFiles) {
+    const event = require(path.join(eventsPath, file));
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args, client, redis));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args, client, redis));
+    }
+  }
+}
+
+client.login(token);
