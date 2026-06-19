0a3b445340c1b9d9bf021b6de44ef2d1eb749eb8
v82rb5wh5g-rgb <v82rb5wh5g@privaterelay.appleid.com>
2026-06-19 06:57:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,9 +35,14 @@ if (fs.existsSync(eventsPath)) {
   }
 }
 
-client.once('ready', () => {
-  console.log('Helper bot ready!');
-  startStatusUpdater(client, redis);
+client.once('ready', async () => {
+  console.log(`${client.user.tag} online`);
+
+  // ---- HEARTBEAT – tells helper we're alive ----
+  await redis.set('bot:heartbeat', Date.now());
+  setInterval(async () => {
+    await redis.set('bot:heartbeat', Date.now());
+  }, 60000); // every minute
 });
 
 client.login(token);
