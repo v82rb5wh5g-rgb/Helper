52af175383714b19964b41a20fce3fb52072ae93
v82rb5wh5g-rgb <v82rb5wh5g@privaterelay.appleid.com>
2026-07-01 05:44:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
   token: process.env.HELPER_TOKEN,
   clientId: process.env.HELPER_CLIENT_ID || "1517368811609788488",
   devId: process.env.DEV_ID || "1303357369622990889",
-  redisUrl: process.env.REDIS_URL || 'redis://default:IujhJsEZHqzRuIukNKiOGGfRsUlpmlcw@yamanote.proxy.rlwy.net:12260'
+  mongoUri: process.env.MONGO_URI'
 };
