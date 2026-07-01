4d1545bbb3ad4675b0edbd005f4d7dde8dcf00fb
v82rb5wh5g-rgb <v82rb5wh5g@privaterelay.appleid.com>
2026-07-01 05:45:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
   token: process.env.HELPER_TOKEN,
   clientId: process.env.HELPER_CLIENT_ID || "1517368811609788488",
   devId: process.env.DEV_ID || "1303357369622990889",
-  mongoUri: process.env.MONGO_URI'
+  mongoUri: process.env.MONGO_URI
 };
